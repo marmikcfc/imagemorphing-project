99d4943c37851399647b47d1907248ad1aadf496
Marmik Pandya <marmikpandya@gmail.com>
2014-11-14 00:42:36 +0530

    Create DestinationFrame.java

---
+++ CODES/DestinationFrame.java
@@ -0,0 +1,29 @@
+import java.awt.Color;
+import javax.swing.JInternalFrame;
+
+public class DestinationFrame extends JInternalFrame
+    implements Constants
+{
+
+    public DestinationFrame(String s, user_intf user_intf)
+    {
+        super(s, true, true, true, true);
+        elementColor = Constants.DEFAULT_ELEMENT_COLOR;
+        elementType = 101;
+        theApp = user_intf;
+    }
+
+    public Color getElementColor()
+    {
+        return elementColor;
+    }
+
+    public int getElementType()
+    {
+        return elementType;
+    }
+
+    private user_intf theApp;
+    private Color elementColor;
+    private int elementType;
+}
